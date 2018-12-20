48db4a8f63bb017d5d4eabfd36386e207a8b8149
Stanislav Baiduzhyi <baiduzhyi.devel@gmail.com>
2018-12-20 12:15:05 +0100

    Covering exceptional cases for both ApiFuture and ListenableFuture.

--- java8-apifuture/src/test/java/net/javacrumbs/futureconverter/java8apifuture/ExceptionTest.java
+++ java8-apifuture/src/test/java/net/javacrumbs/futureconverter/java8apifuture/ExceptionTest.java
@@ -15,25 +15,127 @@
  */
 package net.javacrumbs.futureconverter.java8apifuture;
 
+import com.google.api.core.ApiFutures;
+import com.google.common.util.concurrent.MoreExecutors;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+import java.util.concurrent.ExecutionException;
+
+import static org.hamcrest.core.Is.is;
 
 public class ExceptionTest {
 
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
     @Test
-    public void testException() throws InterruptedException {
+    public void testFinishedRuntimeExceptionStageToApi() throws ExecutionException, InterruptedException {
+        IllegalStateException inputException = new IllegalStateException("something went wrong");
         CompletableFuture<String> future = new CompletableFuture<>();
+        future.completeExceptionally(inputException);
 
-        future.completeExceptionally(new RuntimeException());
-
-        future.thenApply(v-> v).exceptionally(e -> {
-                    System.out.println(e);
-                    return null;
-                });
-
-
-
-        Thread.sleep(100);
+        expectedException.expect(ExecutionException.class);
+        expectedException.expectCause(is(inputException));
+        FutureConverter.toApiFuture(future)
+                .get();
     }
+
+    @Test
+    public void testFinishedCheckedExceptionStageToApi() throws ExecutionException, InterruptedException {
+        Exception inputException = new Exception("something went wrong");
+        CompletableFuture<String> future = new CompletableFuture<>();
+        future.completeExceptionally(inputException);
+
+        expectedException.expect(ExecutionException.class);
+        expectedException.expectCause(is(inputException));
+        FutureConverter.toApiFuture(future)
+                .get();
+    }
+
+    @Test
+    public void testApplyRuntimeExceptionStageToApi() throws ExecutionException, InterruptedException {
+        IllegalStateException inputException = new IllegalStateException("something went wrong");
+        expectedException.expect(ExecutionException.class);
+        expectedException.expectCause(is(inputException));
+
+        FutureConverter.toApiFuture(
+                CompletableFuture.completedFuture("")
+                        .thenApply(s -> {
+                            throw inputException;
+                        }))
+                .get();
+    }
+
+    @Test
+    public void testApplyCheckedExceptionStageToApi() throws ExecutionException, InterruptedException {
+        Exception inputException = new Exception("something went wrong");
+        expectedException.expect(ExecutionException.class);
+        expectedException.expectCause(is(inputException));
+
+        FutureConverter.toApiFuture(
+                CompletableFuture.completedFuture("")
+                        .thenCompose(s -> {
+                            CompletableFuture<String> composed = new CompletableFuture<>();
+                            composed.completeExceptionally(inputException);
+                            return composed;
+                        }))
+                .get();
+    }
+
+    @Test
+    public void testFinishedRuntimeExceptionApiToStage() {
+        IllegalStateException inputException = new IllegalStateException("something went wrong");
+        expectedException.expect(CompletionException.class);
+        expectedException.expectCause(is(inputException));
+
+        FutureConverter.toCompletableFuture(
+                ApiFutures.immediateFailedFuture(inputException))
+                .join();
+    }
+
+    @Test
+    public void testFinishedCheckedExceptionApiToStage() {
+        Exception inputException = new Exception("something went wrong");
+        expectedException.expect(CompletionException.class);
+        expectedException.expectCause(is(inputException));
+
+        FutureConverter.toCompletableFuture(
+                ApiFutures.immediateFailedFuture(inputException))
+                .join();
+    }
+
+    @Test
+    public void testApplyRuntimeExceptionApiToStage() {
+        IllegalStateException inputException = new IllegalStateException("something went wrong");
+        expectedException.expect(CompletionException.class);
+        expectedException.expectCause(is(inputException));
+
+        FutureConverter.toCompletableFuture(
+                ApiFutures.transform(
+                        ApiFutures.immediateFuture(""),
+                        s -> {
+                            throw inputException;
+                        },
+                        MoreExecutors.directExecutor()))
+                .join();
+    }
+
+    @Test
+    public void testApplyCheckedExceptionApiToStage() {
+        Exception inputException = new Exception("something went wrong");
+        expectedException.expect(CompletionException.class);
+        expectedException.expectCause(is(inputException));
+
+        FutureConverter.toCompletableFuture(
+                ApiFutures.transformAsync(
+                        ApiFutures.immediateFuture(""),
+                        s -> ApiFutures.immediateFailedFuture(inputException),
+                        MoreExecutors.directExecutor()))
+                .join();
+    }
+
 }
--- java8-guava/src/test/java/net/javacrumbs/futureconverter/java8guava/ExceptionTest.java
+++ java8-guava/src/test/java/net/javacrumbs/futureconverter/java8guava/ExceptionTest.java
@@ -15,25 +15,126 @@
  */
 package net.javacrumbs.futureconverter.java8guava;
 
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.MoreExecutors;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+import java.util.concurrent.ExecutionException;
+
+import static org.hamcrest.core.Is.is;
 
 public class ExceptionTest {
 
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
     @Test
-    public void testException() throws InterruptedException {
+    public void testFinishedRuntimeExceptionStageToListenable() throws ExecutionException, InterruptedException {
+        IllegalStateException inputException = new IllegalStateException("something went wrong");
         CompletableFuture<String> future = new CompletableFuture<>();
+        future.completeExceptionally(inputException);
 
-        future.completeExceptionally(new RuntimeException());
+        expectedException.expect(ExecutionException.class);
+        expectedException.expectCause(is(inputException));
+        FutureConverter.toListenableFuture(future)
+                .get();
+    }
 
-        future.thenApply(v-> v).exceptionally(e -> {
-                    System.out.println(e);
-                    return null;
-                });
+    @Test
+    public void testFinishedCheckedExceptionStageToListenable() throws ExecutionException, InterruptedException {
+        Exception inputException = new Exception("something went wrong");
+        CompletableFuture<String> future = new CompletableFuture<>();
+        future.completeExceptionally(inputException);
 
+        expectedException.expect(ExecutionException.class);
+        expectedException.expectCause(is(inputException));
+        FutureConverter.toListenableFuture(future)
+                .get();
+    }
 
+    @Test
+    public void testApplyRuntimeExceptionStageToListenable() throws ExecutionException, InterruptedException {
+        IllegalStateException inputException = new IllegalStateException("something went wrong");
+        expectedException.expect(ExecutionException.class);
+        expectedException.expectCause(is(inputException));
 
-        Thread.sleep(100);
+        FutureConverter.toListenableFuture(
+                CompletableFuture.completedFuture("")
+                        .thenApply(s -> {
+                            throw inputException;
+                        }))
+                .get();
+    }
+
+    @Test
+    public void testApplyCheckedExceptionStageToListenable() throws ExecutionException, InterruptedException {
+        Exception inputException = new Exception("something went wrong");
+        expectedException.expect(ExecutionException.class);
+        expectedException.expectCause(is(inputException));
+
+        FutureConverter.toListenableFuture(
+                CompletableFuture.completedFuture("")
+                        .thenCompose(s -> {
+                            CompletableFuture<String> composed = new CompletableFuture<>();
+                            composed.completeExceptionally(inputException);
+                            return composed;
+                        }))
+                .get();
+    }
+
+    @Test
+    public void testFinishedRuntimeExceptionListenableToStage() {
+        IllegalStateException inputException = new IllegalStateException("something went wrong");
+        expectedException.expect(CompletionException.class);
+        expectedException.expectCause(is(inputException));
+
+        FutureConverter.toCompletableFuture(
+                Futures.immediateFailedFuture(inputException))
+                .join();
+    }
+
+    @Test
+    public void testFinishedCheckedExceptionListenableToStage() {
+        Exception inputException = new Exception("something went wrong");
+        expectedException.expect(CompletionException.class);
+        expectedException.expectCause(is(inputException));
+
+        FutureConverter.toCompletableFuture(
+                Futures.immediateFailedFuture(inputException))
+                .join();
+    }
+
+    @Test
+    public void testApplyRuntimeExceptionListenableToStage() {
+        IllegalStateException inputException = new IllegalStateException("something went wrong");
+        expectedException.expect(CompletionException.class);
+        expectedException.expectCause(is(inputException));
+
+        FutureConverter.toCompletableFuture(
+                Futures.transform(
+                        Futures.immediateFuture(""),
+                        s -> {
+                            throw inputException;
+                        },
+                        MoreExecutors.directExecutor()))
+                .join();
+    }
+
+    @Test
+    public void testApplyCheckedExceptionListenableToStage() {
+        Exception inputException = new Exception("something went wrong");
+        expectedException.expect(CompletionException.class);
+        expectedException.expectCause(is(inputException));
+
+        FutureConverter.toCompletableFuture(
+                Futures.transformAsync(
+                        Futures.immediateFuture(""),
+                        s -> Futures.immediateFailedFuture(inputException),
+                        MoreExecutors.directExecutor()))
+                .join();
     }
 }
